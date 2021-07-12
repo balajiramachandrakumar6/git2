c954b103c71989be690c5df728480cc257852017
root <balajiramachandrakumar@gmail.com>
2021-07-12 16:16:17 +0000

    om

---
+++ shell.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+declare -i a
+read -p " Enter the number " aa
+
+while [ "$aa"  -lt  20 ];
+do
+  aa=`expr $aa + 1`
+  echo " $aa "
+  if [ "$aa" -eq 15 ];
+  then
+    mkdir /opt/git
+    cd /opt/git
+    touch om om1
+    echo " directory and file created as category "
+  elif [ "$aa" -eq 11 ];
+  then
+    yum install httpd -y
+    service httpd start
+  else
+    echo " there is nothing "
+    echo " nothing "
+  fi
+done
